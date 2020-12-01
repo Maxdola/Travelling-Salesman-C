0a1b62a7f4b426be9713365eac7f692da547c659
Max Hardtke <Max-Tom@outlook.de>
2020-12-01 10:33:47 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Travelling_Salesman_C.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Travelling_Salesman_C.dir/main.c.o"
+  "Travelling_Salesman_C.exe"
+  "Travelling_Salesman_C.pdb"
+  "libTravelling_Salesman_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Travelling_Salesman_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
